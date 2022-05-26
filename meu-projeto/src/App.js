d7d3ebff3e979afcefe76e2ca59e1d5af8a234b2
Alisson Santana <alisson.santos2@hotmail.com>
2022-05-26 10:27:42 -0300

    Ajuste

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,22 +1,11 @@
-
-import Pessoa from './components/Pessoa';
-import Frase from './components/Frase';
-import List from './components/List';
-
+import Evento from './components/Evento'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Frase/>
-    
-      <Pessoa
-        nome="Alisson"
-        idade="32"
-        profissao="DEV"
-        foto="https://via.placehoder.com/150"
-      />
-      <List/>
+       <h1>Testando Eventos</h1>
+      <Evento />
     </div>
   );
 }
--- meu-projeto/src/components/Evento.js
+++ meu-projeto/src/components/Evento.js
@@ -1,5 +1,15 @@
-function Evento(){
+function Evento() {
 
+    function meuEvento() {
+        console.log('Opa, fui ativado!')
+    }
+
+    return (
+        <div>
+            <p>Clique para disparar em um evento:</p>
+            <button onclick={meuEvento}>Ativar!</button>
+        </div>
+    )
 }
 
 export default Evento
