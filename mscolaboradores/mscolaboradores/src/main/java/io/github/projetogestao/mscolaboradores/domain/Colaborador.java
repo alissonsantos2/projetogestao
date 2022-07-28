c20877c24f3481b73b7bf7220545751407fe83b5
Alisson Santana <alisson.santos2@hotmail.com>
2022-07-28 13:50:11 -0300

    Subindo

---
+++ mscolaboradores/mscolaboradores/src/main/java/io/github/projetogestao/mscolaboradores/domain/Colaborador.java
@@ -0,0 +1,27 @@
+package io.github.projetogestao.mscolaboradores.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Colaborador {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String cpf;
+    private String nome;
+    private Integer idade;
+
+    public Colaborador(String cpf, String nome, Integer idade) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
